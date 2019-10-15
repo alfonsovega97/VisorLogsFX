7b381e7269f7ffc6561e02b1a1dfe25ccdd9e67d
Miguel Fernandez Paradela <miguelfepa@gmail.com>
2019-10-15 17:56:42 +0200

    arreglo v2
    ultimatum de errores

--- src/sample/Controladora.java
+++ src/sample/Controladora.java
@@ -1,18 +1,28 @@
 package sample;
 
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 
-public class Visor extends Main {
+public class Controladora implements Initializable {
 	
-	public static void lector () {
+	@FXML
+	FilteredList lista;
+	@FXML
+	ObservableList<String> list;
+	
+private void lector () {
 		
-		ObservableList<String> list = FXCollections.observableArrayList();
-		File log = new File("src/ArchivoLogs.txt");
+		list = FXCollections.observableArrayList();
+		File log = new File("../ArchivoLogs.txt");
 		Scanner sc;
 		//Bucle que lea hasta el final del archivo 
 	        try {
@@ -27,7 +37,14 @@ public class Visor extends Main {
 	        } catch (Exception e) {
 	            System.out.println(e.getMessage());
 	        }
-	        ListView<String> lista = new ListView<String>(list);
-	        return;
+	        	
 	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		lector();
+		lista=new FilteredList(list);
+		
+	}
+	
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,5 @@
 package sample;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,28 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
+	public static void main(String[] args) {
 		
-		public static void main(String[] args) {
-			Visor lista = new Visor();
-			
-			Visor.lector();
-		}
-
-		@Override
-		public void start(Stage primaryStage) throws Exception {
-			try {
-				
-				Parent root = FXMLLoader.load(getClass().getResource("/src/sample/sample.fxml"));
-				primaryStage.setTitle("Visor de Logs");
-				primaryStage.setScene(new Scene(root));
-				primaryStage.show();
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}
+		launch(args);
 
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("../ventana.fxml"));
+		primaryStage.setTitle("Visor de Logs");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+
+	}
+
+}
